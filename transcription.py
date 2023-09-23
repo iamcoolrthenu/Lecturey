6a2a4ff9ddb679fe5f923cdf87ae06a34a4a97e9
Adams <92267144+adams-x0@users.noreply.github.com>
2023-09-23 15:15:19 -0400

    Update transcription.py
    Signed-off-by: Adams <92267144+adams-x0@users.noreply.github.com>

--- transcription.py
+++ transcription.py
@@ -1,4 +1,4 @@
 import openai
-openai.api_key = "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"
+openai.api_key = "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"
 audio_file= open("Recording.m4a", "rb")
 transcript = openai.Audio.transcribe("whisper-1", audio_file)   
