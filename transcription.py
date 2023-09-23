c8031866a6b441d83d9cb91b82dfbe9f97114284
David Majekodunmi <87044724+davidfmajek@users.noreply.github.com>
2023-09-23 14:38:15 -0400

    Update transcription.py
    Signed-off-by: David Majekodunmi <87044724+davidfmajek@users.noreply.github.com>

--- transcription.py
+++ transcription.py
@@ -1,4 +1,4 @@
 import openai
-openai.api_key = "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"
+openai.api_key = "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"
 audio_file= open("Recording.m4a", "rb")
 transcript = openai.Audio.transcribe("whisper-1", audio_file)   
