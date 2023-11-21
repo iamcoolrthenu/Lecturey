8cd3867f2cf90faf1fe008fa530b85347e436cb8
Coolr <62229244+iamcoolrthenu@users.noreply.github.com>
2023-11-21 17:51:42 -0500

    Update summarize.py
    Signed-off-by: Coolr <62229244+iamcoolrthenu@users.noreply.github.com>

--- summarize.py
+++ summarize.py
@@ -1,12 +1,26 @@
 import openai
 import config
+
+# Set OpenAI API key from the config file
 openai.api_key = config.api_key
 
 def getNotes(transcript):
-  response = openai.Completion.create(
-      engine="text-davinci-002",  # Use the appropriate engine.
-      prompt="Write notes on the following: " + transcript,
-      max_tokens=500,
-      temperature=0.3,
-  )
-  return response['choices'][0]['text']
+    """
+    Generate notes using OpenAI's Completion API.
+
+    Parameters:
+    - transcript (str): The input transcript for generating notes.
+
+    Returns:
+    - str: Generated notes based on the input transcript.
+    """
+    # Create a completion using OpenAI's API
+    response = openai.Completion.create(
+        engine="text-davinci-002",  # Use the appropriate engine.
+        prompt="Write notes on the following: " + transcript,
+        max_tokens=500,
+        temperature=0.3,
+    )
+
+    # Extract and return the generated notes from the API response
+    return response['choices'][0]['text']
